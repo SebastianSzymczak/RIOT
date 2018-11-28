e0459c47ea6182202ae027bb9a757567c11dcff9
Sebastian Szymczak <sebastian.szymczak@haw-hamburg.de>
2018-11-28 16:18:00 +0100

    add Screenshut Subcriber

--- app/shd_subcriber/main.c
+++ app/shd_subcriber/main.c
@@ -17,7 +17,6 @@
 #include "net/ipv6/addr.h"
 #include "periph/gpio.h"
 
-
 #define EMCUTE_PORT         (1883U)
 #define EMCUTE_ID           ("Rudi_Brudi")
 #define EMCUTE_PRIO         (THREAD_PRIORITY_MAIN - 1)
@@ -30,7 +29,6 @@
 #define TOPIC_SUB_LIGHT_2	"subscriber/light_2"
 
 static char stack[THREAD_STACKSIZE_DEFAULT];
-static msg_t queue[8];
 static emcute_sub_t subscriptions[NUMOFSUBS];
 
 
@@ -46,13 +44,13 @@ static void on_pub_light_1(const emcute_topic_t *topic, void *data, size_t len)
 {
     char *in = (char *)data;
 
-    printf("### got publication for topic '%s' [%i] ###\n",
-           topic->name, (int)topic->id);
+    printf(" got publication for topic '%s'\n", topic->name);
     for (size_t i = 0; i < len; i++) {
         printf("%c", in[i]);
     }
-    puts("");
+	puts("");
 	
+	/* Light 1 turn on and Light 2 turn off */
 	gpio_set(GPIO_PIN(0, 23));
 	gpio_clear(GPIO_PIN(0, 28));
 }
@@ -62,13 +60,13 @@ static void on_pub_light_2(const emcute_topic_t *topic, void *data, size_t len)
 {
     char *in = (char *)data;
 
-    printf("### got publication for topic '%s' [%i] ###\n",
-           topic->name, (int)topic->id);
+    printf("got publication for topic '%s' \n", topic->name);
     for (size_t i = 0; i < len; i++) {
         printf("%c", in[i]);
     }
-    puts("");
+	puts("");
 	
+	/* Light 2 turn on and Light 1 turn off */
 	gpio_set(GPIO_PIN(0, 28));
 	gpio_clear(GPIO_PIN(0, 23));
 }
@@ -79,39 +77,33 @@ int main(void)
 	/* Gateway-Structure */
 	sock_udp_ep_t gw = { .family = AF_INET6, .port = EMCUTE_PORT };
 	
-	
-	
     puts("MQTT-SN example application\n");
     puts("Type 'help' to get started. Have a look at the README.md for more"
          "information.");
 	
-	/*	Creating the Gateway address*/
+	/*	Creating address*/
 	if (ipv6_addr_from_str((ipv6_addr_t *)&gw.addr.ipv6, BROKER_IP) == NULL) {
         printf("error parsing IPv6 address\n");
         return 1;
     }
 	
-	// LED 1 OUTPUT
+	// LED 1 initialize to OUTPUT
     if (gpio_init(GPIO_PIN(0, 23),  GPIO_OUT) < 0) {
         printf("Error to initialize GPIO_PIN(%i, %02i)\n", 0, 23);
         return 1;
     }
+	
 	// LED 2 OUTPUT
 	if (gpio_init(GPIO_PIN(0, 28),  GPIO_OUT) < 0) {
         printf("Error to initialize GPIO_PIN(%i, %02i)\n", 0, 28);
         return 1;
     }
 	
-    /* the main thread needs a msg queue to be able to run `ping6`*/
-    msg_init_queue(queue, (sizeof(queue) / sizeof(msg_t)));
-    /* initialize our subscription buffers */
-    memset(subscriptions, 0, (NUMOFSUBS * sizeof(emcute_sub_t)));
-	
     /* start the emcute thread */
     thread_create(stack, sizeof(stack), EMCUTE_PRIO, 0,
                   emcute_thread, NULL, "emcute");
 	printf("Trying to connect\n");	
-	
+
 	/* Waiting time for global address can be generated  */
 	xtimer_sleep(5);
 	/* Function to establish a connection */
@@ -121,18 +113,20 @@ int main(void)
 	}			  
 	printf("Successfully connected to Gateway\n");
 		  
+	/*	*/	  
 	subscriptions[0].cb = on_pub_light_1;
 	subscriptions[0].topic.name = TOPIC_SUB_LIGHT_1;
-	/* Sign in to subcribe */
+	/* Sign in to subscribe */
 	if (emcute_sub(&subscriptions[0], EMCUTE_QOS_0) != EMCUTE_OK) {
 		printf("error: unable to subscribe to Gateway light 1\n");
 		return 1;
 	}
     printf("Now subscribed to Gateway light 1\n");
 	
+	/*	*/
 	subscriptions[1].cb = on_pub_light_2;
     subscriptions[1].topic.name = TOPIC_SUB_LIGHT_2;
-	/* Sign in to subcribe */
+	/* Sign in to subscribe */
 	if (emcute_sub(&subscriptions[1], EMCUTE_QOS_0) != EMCUTE_OK) {
 		printf("error: unable to subscribe to Gateway light 2\n");
 		return 1;
