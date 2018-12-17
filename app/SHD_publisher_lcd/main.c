23eb053d79db9b58245b66f18778855db5e11fe2
Sebastian Szymczak <sebastian.szymczak@haw-hamburg.de>
2018-12-17 13:56:56 +0100

    Display connected and functional

--- app/SHD_publisher_lcd/main.c
+++ app/SHD_publisher_lcd/main.c
@@ -171,8 +171,8 @@ static uint32_t pins_enabled = (
 );
 #endif
 
-
 static const char MICRO_CONTROLLER_1[] = "MICRO_CONTROLLER_1";
+
 
 // emcute part
 static void *emcute_thread(void *arg)
@@ -183,90 +183,6 @@ static void *emcute_thread(void *arg)
 }
 
 
-/*static unsigned get_qos(const char *str)
-{
-    int qos = atoi(str);
-    switch (qos) {
-        case 1:     return EMCUTE_QOS_1;
-        case 2:     return EMCUTE_QOS_2;
-        default:    return EMCUTE_QOS_0;
-    }
-}*/
-
-/*static int cmd_con(int argc, char **argv)
-{
-    sock_udp_ep_t gw = { .family = AF_INET6, .port = EMCUTE_PORT };
-    char *topic = NULL;
-    char *message = NULL;
-    size_t len = 0;
-
-    if (argc < 2) {
-        printf("usage: %s <ipv6 addr> [port] [<will topic> <will message>]\n", argv[0]);
-        return 1;
-    }
-
-     parse address 
-    if (ipv6_addr_from_str((ipv6_addr_t *)&gw.addr.ipv6, argv[1]) == NULL) {
-        printf("error parsing IPv6 address\n");
-        return 1;
-    }
-
-    if (argc >= 3) {
-        gw.port = atoi(argv[2]);
-    }
-    if (argc >= 5) {
-        topic = argv[3];
-        message = argv[4];
-        len = strlen(message);
-    }
-
-    if (emcute_con(&gw, true, topic, message, len, 0) != EMCUTE_OK) {
-        printf("error: unable to connect to [%s]:%i\n", argv[1], (int)gw.port);
-        return 1;
-    }
-    printf("Successfully connected to gateway at [%s]:%i\n",
-           argv[1], (int)gw.port);
-    return 0;
-}*/
-
-
-/*static int cmd_pub(int argc, char **argv)
-{
-    emcute_topic_t t;
-    unsigned flags = EMCUTE_QOS_0;
-
-    if (argc < 3) {
-        printf("usage: %s <topic name> <data> [QoS level]\n", argv[0]);
-        return 1;
-    }
-
-     parse QoS level 
-    if (argc >= 4) {
-        flags |= get_qos(argv[3]);
-    }
-
-    printf("pub with topic: %s and name %s and flags 0x%02x\n", argv[1], argv[2], (int)flags);
-
-     step 1: get topic id 
-    t.name = argv[1];
-    if (emcute_reg(&t) != EMCUTE_OK) {
-        puts("error: unable to obtain topic ID");
-        return 1;
-    }
-
-     step 2: publish data 
-    if (emcute_pub(&t, argv[2], strlen(argv[2]), flags) != EMCUTE_OK) {
-        printf("error: unable to publish data to topic '%s [%i]'\n",
-                t.name, (int)t.id);
-        return 1;
-    }
-
-    printf("Published %i bytes to topic '%s [%i]'\n",
-            (int)strlen(argv[2]), t.name, t.id);
-
-    return 0;
-}*/
-
 
 char* concat(const char *s1, const char *s2)
 {
@@ -276,127 +192,6 @@ char* concat(const char *s1, const char *s2)
     strcat(result, s2);
     return result;
 }
-
-/*static int raspberry_pi(int sensor1 , int sensor2){
-	
-
-
-	            // send the message to the controller mc as raspberry_pi(int sensor1 , int sensor2)
-                if(sensor1 && sensor2){
-					  gpio_set(GPIO_PIN(0,23));
-					  gpio_set(GPIO_PIN(0,28));
-					  char *message[] = {"pub","publisher/bothOn","1"};
-						 cmd_pub(3, message);
-				  }
-				// Reading from the infrared sensor  1
-				   else if (sensor1) {
-		                 printf("Movement has been detected in room 1\n");
-						 roomNumber = '1';
-				         gpio_set(GPIO_PIN(0,23));
-						 gpio_clear(GPIO_PIN(0,28));
-						 	char *message[] = {"pub","publisher/roomSensorInformation1","1"};
-						 cmd_pub(3, message);
-                           }
-				// Reading from the infrared sensor  2  
-		            else if (sensor2) {
-		                 printf("Movement has been detected in room 2\n");
-						 roomNumber = '2';
-						 gpio_set(GPIO_PIN(0,28));
-				         gpio_clear(GPIO_PIN(0,23));
-						 char *message[] = {"pub","publisher/roomSensorInformation2","1"};
-						 cmd_pub(3, message);
-                           }
-						    xtimer_sleep(2);	
-						   return 0;
-};
-
-void *adc(void *argv){
-
-    printf("%s is ON \n" , MICRO_CONTROLLER_1);
-    xtimer_ticks32_t last = xtimer_now();
-    int sample = 0;
-
-    puts("\nRIOT ADC peripheral driver test\n");
-    puts("This test will sample all available ADC lines once every 100ms with\n"
-         "a 10-bit resolution and print the sampled results to STDIO\n\n");
- 
-    for (unsigned i = 0; i < ADC_NUMOF; i++) {
-        if (adc_init(ADC_LINE(i)) < 0) {
-            printf("Initialization of ADC_LINE(%u) failed\n", i);
-            return NULL;;
-        } else {
-            printf("Successfully initialized ADC_LINE(%u)\n", i);
-        }
-    }
-	
-	// Sensor 1 INPUT
-	 if (gpio_init(GPIO_PIN(0, 13),  GPIO_IN) < 0) {
-        printf("Error to initialize GPIO_PIN(%i, %02i)\n", 0, 13);
-        return NULL;;
-    }
-	
-	// Sensor 2 INPUT
-	 if (gpio_init(GPIO_PIN(0, 14),  GPIO_IN) < 0) {
-        printf("Error to initialize GPIO_PIN(%i, %02i)\n", 0, 14);
-        return NULL;;
-    }
-	
-	
-	 
-    while (1) {
-        for (unsigned i = 0; i < ADC_NUMOF; i++) {
-            sample = adc_sample(ADC_LINE(i), RES);
-            if (sample < 0) {
-                printf("ADC_LINE(%u): 10-bit resolution not applicable\n", i);
-            } else {
-                printf("ADC_LINE(%u): %i\n", i, sample);
-            }
-			
-			if(i == 0) {
-			if(sample > 50){
-				printf("Someone on the door!! \n");
-				xtimer_sleep(1);
-				printf("ADC value is %d \n", sample);
-				int sensor1 = gpio_read(GPIO_PIN(0, 13));
-				int sensor2 = gpio_read(GPIO_PIN(0, 14));
-				
-				This code is for the raspberry pi
-            raspberry_pi(sensor1 , sensor2); //this should be sent to rpi as message
-			}
-			This code is for the raspberry pi
-			}
-        }
-        xtimer_periodic_wakeup(&last, DELAY);
-    }
-  
-   (void)(argv);
-  
-return NULL;;
-}
-
-void *timeLEDIsOn(void *arg)
-{
-	(void) arg;
-	  while(1){
-		  if(roomNumber == '1'){
-			  xtimer_sleep(1);
-			  roomsTimeArray[0]++;
-		  }
-			  else if(roomNumber == '2'){
-				  xtimer_sleep(1);
-				  roomsTimeArray[1]++;
-				  
-			  }
-			  
-			   else{
-				  xtimer_sleep(1);	  
-			  }
-	     
-		  printf("Room 1 was on for:  %d \n", roomsTimeArray[0]);
-		  printf("Room 2 was on for:  %d \n", roomsTimeArray[1]);
-	  }
-	    return 0;
-}*/
 
 
 
@@ -422,9 +217,7 @@ void *monitorThread(void *arg)
 {
 	(void) arg;
     uint32_t screen = 0;
-    u8g2_t u8g2;
-
-	    
+    u8g2_t u8g2;  
 					
     /* initialize to stdout */
 #if TEST_OUTPUT == TEST_OUTPUT_STDOUT
@@ -515,89 +308,26 @@ void *monitorThread(void *arg)
 }
 
 
-/*void functionMCSensor (void){
-	  the main thread needs a msg queue to be able to run `ping6`
-    msg_init_queue(queue, (sizeof(queue) / sizeof(msg_t)));
-
-     initialize our subscription buffers 
-    memset(subscriptions, 0, (NUMOFSUBS * sizeof(emcute_sub_t)) );
-
-     start the emcute thread 
-    thread_create(stack4, sizeof(stack4), EMCUTE_PRIO, 0,
-                  emcute_thread, NULL, "emcute");
-
-		    char *args[] = {"con",BROCKER_IP,BROCKER_PORT,"",""};				
-					 if(!cmd_con(5, args)){
-						 puts("connection has been established");
-					char *message[] = {"pub","publisher/communication", "\"The SHD microcontroller is live, and ready to communicate.\""};
-						 cmd_pub(3, message);
-					 }
-					 else{ puts("error in connection!!!"); }
-	return ;
-}*/
-
-
-/*void functionMCController (void){
-		
-	// LED 1 OUTPUT
-    if (gpio_init(GPIO_PIN(0, 23),  GPIO_OUT) < 0) {
-        printf("Error to initialize GPIO_PIN(%i, %02i)\n", 0, 23);
-        return ;
-    }
-	// LED 2 OUTPUT
-	if (gpio_init(GPIO_PIN(0, 28),  GPIO_OUT) < 0) {
-        printf("Error to initialize GPIO_PIN(%i, %02i)\n", 0, 28);
-        return ;
-    }
-	    // Turn off the LED 1 as first state
-	    gpio_clear(GPIO_PIN(0,23)); 
-	    gpio_clear(GPIO_PIN(0,28)); 
-	
-		thread_create(stack1, sizeof(stack1),
-                    THREAD_PRIORITY_MAIN - 1,
-                    THREAD_CREATE_STACKTEST,
-                    monitorThread,
-                    NULL, "monitorThread");
-					
-		thread_create(stack3, sizeof(stack3),
-                    THREAD_PRIORITY_MAIN - 1,
-                    THREAD_CREATE_STACKTEST,
-                    timeLEDIsOn,
-                    NULL, "timeLEDIsOn");
-	return ;
-}*/
-
-
-/*int main(void)
-{
-	functionMCSensor(); // function to simulate sensor mc
-	functionMCController(); // function to simulate the controller mc
-		
-    return 0;
-}*/
-
 
 static void on_pub_light_1(const emcute_topic_t *topic, void *data, size_t len)
 {
     char *in = (char *)data;
-
-    printf(" got publication for topic '%s'\n", topic->name);
+	
+    printf("got publication for topic '%s'", topic->name);
     for (size_t i = 0; i < len; i++) {
-        printf("%c", in[i]);
+        printf("   %c", in[i]);
     }
 	puts("");
 	
 	/* Light 1 turn on and Light 2 turn off */
 	if (in[0] == '1')
-	{gpio_set(GPIO_PIN(0, 23));}
+		{gpio_set(GPIO_PIN(0, 23));
+			roomNumber= '1';}
 	else if (in[0] == '0'){
-		gpio_clear(GPIO_PIN(0, 23));
-	}
+		gpio_clear(GPIO_PIN(0, 23));}
 	else {printf("error: got wrong publication for topic\n");}
 	
 
-	//gpio_set(GPIO_PIN(0, 23));
-	//gpio_clear(GPIO_PIN(0, 28));
 }
 
 
@@ -605,21 +335,45 @@ static void on_pub_light_2(const emcute_topic_t *topic, void *data, size_t len)
 {
     char *in = (char *)data;
 
-    printf("got publication for topic '%s' \n", topic->name);
+    printf("got publication for topic '%s'", topic->name);
     for (size_t i = 0; i < len; i++) {
-        printf("%c", in[i]);
+        printf("   %c", in[i]);
     }
 	puts("");
 	
 	/* Light 1 turn on and Light 2 turn off */
 	if (in[0] == '1')
-	{gpio_set(GPIO_PIN(0, 28));}
+		{gpio_set(GPIO_PIN(0, 28));
+		 roomNumber= '2';
+		}
 	else if (in[0] == '0'){
-		gpio_clear(GPIO_PIN(0, 28));
-	}
+		gpio_clear(GPIO_PIN(0, 28));}
 	else {printf("error: got wrong publication for topic\n");}
 }
 
+void *timeLEDIsOn(void *arg)
+{
+	(void) arg;
+	  while(1){
+		  if(roomNumber == '1'){
+			  xtimer_sleep(1);
+			  roomsTimeArray[0]++;
+		  }
+			  else if(roomNumber == '2'){
+				  xtimer_sleep(1);
+				  roomsTimeArray[1]++;
+				  
+			  }
+			  
+			   else{
+				  xtimer_sleep(1);	  
+			  }
+	     
+		 // printf("Room 1 was on for:  %d \n", roomsTimeArray[0]);
+		  //printf("Room 2 was on for:  %d \n", roomsTimeArray[1]);
+	  }
+	    return 0;
+}
 
 int main(void)
 {
@@ -659,12 +413,12 @@ int main(void)
                     monitorThread,
                     NULL, "monitorThread");
 					
-		/*thread_create(stack3, sizeof(stack3),
+	thread_create(stack3, sizeof(stack3),
                     THREAD_PRIORITY_MAIN - 1,
                     THREAD_CREATE_STACKTEST,
                     timeLEDIsOn,
-                    NULL, "timeLEDIsOn");*/
-
+                    NULL, "timeLEDIsOn");
+					
 	/* Waiting time for global address can be generated  */
 	xtimer_sleep(5);
 	/* Function to establish a connection */
